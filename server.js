e1a268b846e36754ef24a044b53f108832a4ba7d
spexzee <abub5979@gmail.com>
2025-02-07 20:06:42 +0530

    cors error

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE","PATCH"],
+    credentials: true,
+  })
+);
 
 //router
 app.use('/auth', AuthRoutes)

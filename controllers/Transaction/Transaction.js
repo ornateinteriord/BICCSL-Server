835d16d83a7e501cf3a59c86b4141ef9100292ac
Ornate Interior Decor <ornateinteriord@gmail.com>
2025-02-12 21:52:06 +0530

    Revert "Revert "Added JWtokens and Authorization Role""

--- controllers/Transaction/Transaction.js
+++ controllers/Transaction/Transaction.js
@@ -1,16 +1,28 @@
 const  mongoose  = require("mongoose");
 const TransactionModel = require("../../models/Transaction/Transaction");
-const MemberModel = require("../../models/Users/Member");
+
 
 const getTransactionDetails = async (req, res) => {
   try {
     const { id } = req.params;
+    const loggedInUserId = req.user.id;
+    const userRole = req.user.role;
     
-    if (!mongoose.Types.ObjectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(loggedInUserId)) {
         return res.status(400).json({ success: false, message: "Invalid User ID" });
       }
-   
-    const transactions = await TransactionModel.find({userId:id});
+       
+      if (userRole === "ADMIN") {
+        const query = id ? { userId: id } : {}; 
+        const transactions = await TransactionModel.find(query);
+  
+        return res.status(200).json({ success: true, data: transactions });
+      }
+
+      if (userRole === "USER" && id !== loggedInUserId) {
+        return res.status(403).json({ success: false, message: "transactions Access denied" });
+      }
+    const transactions = await TransactionModel.find({userId:loggedInUserId});
     if (transactions.length === 0) {
         return res.status(404).json({ success: false, message: "No transactions found for this user" });
       }
